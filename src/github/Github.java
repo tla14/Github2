ddcf53886e81654f452e849ef13162b0d8b1656c
tla14@students.ptcollege.edu <>
2020-02-04 15:14:23 -0500

    this is the first commit

---
+++ src/github/Github.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package github;
+
+/**
+ *
+ * @author ikoal
+ */
+public class Github {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Ayres();
+    }
+    public static void Ayres(){
+        System.out.println("Tracy Ayres");
+    }
+}
